dc81c860a5f78e991b50de18c18103a55df1974e
Alex Santana <alexstpr@gmail.com>
2023-05-12 18:37:52 -0300

    Ajustes Layout

--- Front/ProEventos-App/src/app/shared/titulo/titulo.component.ts
+++ Front/ProEventos-App/src/app/shared/titulo/titulo.component.ts
@@ -7,6 +7,9 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class TituloComponent implements OnInit {
   @Input() titulo: string = "";
+  @Input() subtitulo = 'Desde 2023';
+  @Input() iconClass = 'fa fa-user';
+  @Input() botaolistar = false;
   constructor() { }
 
   ngOnInit() {
